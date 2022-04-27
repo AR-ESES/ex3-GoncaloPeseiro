42892288e5a52b877e2d762df0bf7c114fce9d95
GoncaloPeseiro <102664610+GoncaloPeseiro@users.noreply.github.com>
2022-04-27 22:48:08 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,29 @@
 let laranjas;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(0)
+    createCanvas(windowWidth, windowHeight);
+    background(0);
 
 }
 
 function draw() {
 
-	stroke(230);
-	strokeWeight(2);
-	noFill();
-	circle(width / 2, height / 2, height / 4);
+    background(0)
 
+    if(mouseY>=height/2 && mouseX<=width/2)
+    fill    (64,224,208); // azul
 
+    else if(mouseY<height/2 && mouseX<=width/2) 
+    fill    (255,99,71); // vermelho
+
+    else if(mouseY<height/2 && mouseX>width/2) 
+    fill    (0,0,139); // verde 
+
+    else{
+         noFill();
+    }
+
+    stroke(230);
+    strokeWeight(2);
+    circle(width / 2, height / 2, height / 4);
 }
